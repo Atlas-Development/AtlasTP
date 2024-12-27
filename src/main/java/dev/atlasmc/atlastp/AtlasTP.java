5ffcbde5ff55fbbd38697e62c0b7a0d04b77013d
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2024-12-27 17:29:12 +0100

    Initialize Repository
    Setup License, Gradle and Project Structure

---
+++ src/main/java/dev/atlasmc/atlastp/AtlasTP.java
@@ -0,0 +1,55 @@
+package dev.atlasmc.atlastp;
+
+import com.google.inject.Inject;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.api.Server;
+import org.spongepowered.api.command.Command;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.lifecycle.ConstructPluginEvent;
+import org.spongepowered.api.event.lifecycle.RegisterCommandEvent;
+import org.spongepowered.api.event.lifecycle.StartingEngineEvent;
+import org.spongepowered.api.event.lifecycle.StoppingEngineEvent;
+import org.spongepowered.plugin.PluginContainer;
+import org.spongepowered.plugin.builtin.jvm.Plugin;
+
+/**
+ * The main class of your Sponge plugin.
+ *
+ * <p>All methods are optional -- some common event registrations are included as a jumping-off point.</p>
+ */
+@Plugin("atlastp")
+public class AtlasTP {
+
+    private final PluginContainer container;
+    private final Logger logger;
+
+    @Inject
+    AtlasTP(final PluginContainer container, final Logger logger) {
+        this.container = container;
+        this.logger = logger;
+    }
+
+    @Listener
+    public void onConstructPlugin(final ConstructPluginEvent event) {
+        // Perform any one-time setup
+        this.logger.info("Constructing AtlasTP");
+    }
+
+    @Listener
+    public void onServerStarting(final StartingEngineEvent<Server> event) {
+        // Any setup per-game instance. This can run multiple times when
+        // using the integrated (singleplayer) server.
+    }
+
+    @Listener
+    public void onServerStopping(final StoppingEngineEvent<Server> event) {
+        // Any tear down per-game instance. This can run multiple times when
+        // using the integrated (singleplayer) server.
+    }
+
+    @Listener
+    public void onRegisterCommands(final RegisterCommandEvent<Command.Parameterized> event) {
+        // Register a simple command
+        // When possible, all commands should be registered within a command register event
+    }
+}
